42c5c17181548fc729f96a69c3faea4ac8cba046
higashijima <boctok.ctpoba@gmail.com>
2019-02-04 21:34:48 +0900

    GPIO output for relay control pin
    When the value of distance sensor is 2048, high and low signals of the pin are exchanged

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from modules.mcp3208 import MCP3208
+import RPi.GPIO as GPIO
+
+RELAT_CTL = 18
 
 def main():
   ADC = MCP3208(11, 10, 9, 8)
+  GPIO.setup(RELAY_CTL, GPIO.OUT)
   while True:
     for ch in range(2):
       value = ADC.adc(ch)
-      print("{}:{}".format(ch, value))
-
-
+      if vale < 2048:
+        GPIO.output(RELAY_CTL, GPIO.HIGH)
+      else:
+        GPIO.output(RELAY_CTL, GPIO.LOW)
+        
 if __name__ == '__main__':
   main()
 
